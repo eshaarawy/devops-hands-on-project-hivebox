4795111f8371cffd29193748d806926150ee7d83
Mohamed Elshaarawy <elsharawy234@gmail.com>
2025-08-06 00:36:22 +0300

    Add metrics endpoint

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const { getVersion } = require('./version');
 const axios = require('axios');
+const client = require('prom-client');
+
+const { getVersion } = require('./version');
+
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
 const SENSEBOX_API_BASE_URL = process.env.SENSEBOX_API_BASE_URL;
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
 
 app.get('/version', (req, res) => {
   const version = getVersion();
@@ -59,16 +64,29 @@ app.get('/temperature', async (req, res) => {
           ? temperatureData.reduce((sum, temp) => sum + temp, 0) / temperatureData.length 
           : null;
 
+        const status = averageTemperature < 10
+          ? 'Too Cold'
+          : averageTemperature <= 36
+          ? 'Good'
+          : 'Too Hot';
+
         res.json({
           boxes: filteredBoxes,
           totalSensors: temperatureData.length,
-          averageTemperature: averageTemperature ? Math.round(averageTemperature * 100) / 100 : null
+          averageTemperature: averageTemperature ? Math.round(averageTemperature * 100) / 100 : null,
+          status
         });
 
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
     });
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  let metrics = await register.metrics();
+  res.send(metrics);
+});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
