6f862be5089f204f81e5200c1bd9a0da934c8a36
Mohamed Elshaarawy <89318263+eshaarawy@users.noreply.github.com>
2025-08-15 00:17:07 +0300

    Phase 5 (#6)
    * Update .gitignore
    
    * Add terraform script

---
+++ eks-terraform/eks.tf
@@ -0,0 +1,33 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.17.2"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.30"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = ["t3.medium"]
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+# Output EKS cluster details
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_name" {
+  description = "EKS cluster name"
+  value       = module.eks.cluster_name
+}
---
+++ eks-terraform/vpc.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.1.2"
+
+  name = "eks-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnet_cidrs
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  enable_dns_hostnames   = true
+  enable_dns_support     = true
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = "1"
+  }
+}
